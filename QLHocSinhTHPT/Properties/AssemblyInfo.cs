b13b96ed0e117451e1225ab447fa657707f576ad
LongDuongBIS <huulongduong@gmail.com>
2017-03-24 06:42:51 +0700

    Xong

---
+++ QLHocSinhTHPT/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QLHocSinhTHPT")]
+[assembly: AssemblyDescription("Phần mềm Quản lý Điểm Học sinh THPT Cát Tiên")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("University of Economics HCMC - Faculty of Business Information System")]
+[assembly: AssemblyProduct("Cat Tien School Management System")]
+[assembly: AssemblyCopyright("Copyright © 2017 Dương Hữu Long")]
+[assembly: AssemblyTrademark("Copyright © 2017 Dương Hữu Long")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f01f7024-ec80-40e5-acfc-c848e0af1d38")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
